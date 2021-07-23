0beb004dd5c9849ffc19223dc674cb64f7c4f324
Samuel Fernandes <samueldiablo73@gmail.com>
2021-07-23 14:31:02 -0300

    react inicializado

---
+++ shop/serializers.py
@@ -0,0 +1,22 @@
+
+from rest_framework import serializers
+from .models import *
+
+
+class ProductSerialializers(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+        depth = 1
+
+
+    def imageurl(self, obj):
+        request = self.context.get('request')
+        return request.url(image)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
